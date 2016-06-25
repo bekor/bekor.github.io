8101b10c166217cfd9111a4d07e180cd8bab43f0
bekor <bekor88@gmail.com>
2016-06-26 01:24:05 +0200

    prjpython.html and dropdown menu

--- js/main.js
+++ js/main.js
@@ -2,21 +2,12 @@
 
 /* global x */
 
-function displayToggle() {
+function projectsToggle() {
     var oggle = document.getElementById('projectsId');
     if (oggle.style.display === 'none') {
         oggle.style.display = 'inline';
     } else {
         oggle.style.display = 'none';
     }
-
-    /*$('.pbuttons').click(function() {
-        $('.visible').animate({
-           left: "0px" 
-        }, 200);
-    });*/
-        
-
 }
 
-//$(document).ready(slideup);
